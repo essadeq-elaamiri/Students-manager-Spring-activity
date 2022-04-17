1017d5fa03125e7bb0053957f9cd806c10d979cb
edssadeq <essadeq0701@gmail.com>
2022-04-17 12:18:37 +0000

    static/css/ .. problems

--- src/main/java/com/elaamiri/studentsmanager/webControllers/StudentController.java
+++ src/main/java/com/elaamiri/studentsmanager/webControllers/StudentController.java
@@ -1,10 +1,14 @@
 package com.elaamiri.studentsmanager.webControllers;
 
+import com.elaamiri.studentsmanager.entities.Student;
 import com.elaamiri.studentsmanager.services.StudentService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Data
@@ -20,5 +24,19 @@ public class StudentController {
     @GetMapping("/")
     public String handleRootPath(){
         return "redirect:/home";
+    }
+
+    @GetMapping("/user/students")
+    public String showStudentsList(Model model,
+                                   @RequestParam(name="keyword", defaultValue = "") String keyword,
+                                   @RequestParam(name="page",defaultValue = "0") int page,
+                                   @RequestParam(name="size",defaultValue = "7") int size)
+    {
+        Page<Student> studentsPage = studentService.getStudentsList(keyword, page, size);
+        model.addAttribute("studentsLis", studentsPage.getContent());
+        if(studentsPage.isEmpty()){
+            model.addAttribute("message", "No result found !");
+        }
+        return "/student/studentsList";
     }
 }
